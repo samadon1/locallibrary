2921f7931633871abc957c12adba9f29e08d4510
Samuel Donkor <56901167+samadon1@users.noreply.github.com>
2022-07-23 10:53:23 +0000

    Add files via upload

---
+++ catalog/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from .models import Author, Book, BookInstance, Genre
+from django.views import generic
+
+# Create your views here.
+def index(request):
+
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_authors': num_authors,
+        'nums_instances_available': num_instances_available,
+    }
+
+    return render(request, 'catalog/index.html', context)
+
+def books(request):
+    books = Book.objects.all()
+    context = {
+        'books': books
+    }
+
+
+    return render(request, 'catalog/books.html', context)
+
+def bookDetails(request, pk):
+    book = Book.objects.get(id=pk)
+    context = {
+        'book': book
+    }
+
+
+    return render(request, 'catalog/bookDetails.html', context)
+
+def authors(request):
+    authors = Author.objects.all()
+    context = {
+        'authors': authors
+    }
+
+
+    return render(request, 'catalog/authors.html', context)
